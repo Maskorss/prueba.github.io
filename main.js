c38d16eb32692165737e8318f85431a19381f369
Maskorss <141846460+Maskorss@users.noreply.github.com>
2023-08-12 17:37:48 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
+    // Verifica las credenciales aquí (esto es solo un ejemplo)
+    if (username === "usuario" && password === "contraseña") {
+        window.location.href = "contenido-secreto.html";
+    } else {
+        alert("Credenciales incorrectas. Por favor, inténtalo nuevamente.");
+    }
+});
